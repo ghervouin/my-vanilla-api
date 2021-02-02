3d475ffdc14ad36543cf82caafa6908020800cd3
ghervouin <54936763+ghervouin@users.noreply.github.com>
2021-02-02 19:19:51 +0100

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,7 +1,5 @@
 module.exports = async function (context, req) {
     context.res = {
-      body: { 
-        text: "Hello from the API" 
-      }
+      body: {text: "Hello from the API"}
     };
   };
